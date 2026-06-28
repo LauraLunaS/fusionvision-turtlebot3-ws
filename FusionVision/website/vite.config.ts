a95b9826302fa2c6e851b78fd2d97617f5c5d56f
LauraLunaS <lauraluna.siqueira@hotmail.com>
2026-06-28 20:42:27 -0300

    fix: substitui FusionVision pela versão correta utilizada no projeto

--- FusionVision/website/vite.config.ts
+++
@@ -1,9 +0,0 @@
-// website/vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/FusionVision/' // IMPORTANT: Set this to your repository name
-})
